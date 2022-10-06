fb046e486c06b949e4b20682b3808f1928e3f91a
underscoreDev <godswill@zimmerman.team>
2022-10-06 14:02:10 +0100

    feat: quick fixes

--- udagram-api/src/controllers/v0/feed/models/FeedItem.ts
+++ udagram-api/src/controllers/v0/feed/models/FeedItem.ts
@@ -10,14 +10,11 @@ import {
 export class FeedItem extends Model<FeedItem> {
   @Column
   public caption!: string;
-
   @Column
   public url!: string;
-
   @Column
   @CreatedAt
   public createdAt: Date = new Date();
-
   @Column
   @UpdatedAt
   public updatedAt: Date = new Date();
